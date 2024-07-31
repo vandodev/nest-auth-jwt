7a22f851c45bd9ed4b2c1e96d5fcb6095fff2235
vandodev <vandoaparecido@hotmail.com>
2024-07-31 10:13:18 -0300

    :unlock: :key: :lock: Nest g service auth/jwt-strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { JwtStrategyService } from './jwt-strategy/jwt-strategy.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategyService]
 })
 export class AuthModule {}
