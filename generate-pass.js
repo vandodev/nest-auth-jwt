c0cac777665c6af868b1ff222bdbede56483087f
vandodev <vandoaparecido@hotmail.com>
2024-07-31 07:40:46 -0300

    :unlock: :key: :lock: Script para geração de senha

---
+++ generate-pass.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const password = process.argv.slice(2)[0];
+
+bcrypt.genSalt(saltRounds, function (err, salt) {
+  bcrypt.hash(password, salt, function (err, hash) {
+    console.log(hash);
+  });
+});
+
+//execute no terminal
+//node generate-pass.js senhaAqui
